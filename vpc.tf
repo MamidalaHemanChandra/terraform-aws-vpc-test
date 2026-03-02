20f01a0fa2f7fa09c6680671df409b43ff24c1f2
heman chandra <hemanchandra701@gmail.com>
2026-03-02 22:20:19 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -8,5 +8,5 @@ module "vpc" {
     public_subnet_cidr_block = var.public_subnet_cidr_block
     private_subnet_cidr_block = var.private_subnet_cidr_block
     database_subnet_cidr_block = var.database_subnet_cidr_block
-    # is_perring_requried = false
+    is_perring_requried = true
 }
