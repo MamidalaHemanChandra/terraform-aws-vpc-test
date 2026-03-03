d30ee99a685b71e02c1199ef0d1a54c9c8adb59d
heman chandra <hemanchandra701@gmail.com>
2026-03-03 19:08:10 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
-    # source = "../terraform-aws-vpc"
-    source = "git::https://github.com/MamidalaHemanChandra/terraform-aws-vpc.git"
+    source = "../terraform-aws-vpc"
     vpc_cidr_block = var.vpc_cidr_block
     project = var.project
     environment = var.environment
@@ -8,5 +7,5 @@ module "vpc" {
     public_subnet_cidr_block = var.public_subnet_cidr_block
     private_subnet_cidr_block = var.private_subnet_cidr_block
     database_subnet_cidr_block = var.database_subnet_cidr_block
-    is_perring_requried = is_perring_requried
+    peering_required = true
 }
